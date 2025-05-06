287772799b40445241e6b57660b3c8854edac0a4
jorgeCastAdan <jcastellanosadan@gmail.com>
2025-05-06 15:37:20 +0200

    Delete src directory

--- src/app/core/directivas/directiva-ejemplo.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirectivaEjemploDirective } from './directiva-ejemplo.directive';
-
-describe('DirectivaEjemploDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirectivaEjemploDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/core/interceptores/interceptor-ejemplop.interceptor.ts
+++
@@ -1,5 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const interceptorEjemplopInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
