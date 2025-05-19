07c64a73fad94e89bbdfec95af89697f2d6a3296
Jorge Castellanos <jcastellanosadan@gmail.com>
2025-05-19 14:41:27 +0200

    todas actividades / mis actividades v2.0

--- src/app/componentes/actividades/actividades.component.ts
+++ src/app/componentes/actividades/actividades.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActividadesService } from '../../core/services/actividades.service';
 import { Actividad } from '../../core/tipados';
 import { NgFor } from '@angular/common';
 import { CortarDescPipe } from './cortar-desc.pipe';
 import {MatDividerModule} from '@angular/material/divider';
 import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-actividades',
@@ -14,15 +15,33 @@ import { Router } from '@angular/router';
   styleUrl: './actividades.component.css'
 })
 export class ActividadesComponent {
+  private auth = inject(AuthService)
+
+  todasActividades: boolean = true;
+
   actividades:Actividad[];
+
+  actividadesUsuario: Actividad[] = []
+
+  email: string= this.auth.usuario?.email!;
 
   constructor(private http:ActividadesService, private router:Router){
     this.actividades = []
-    this.http.getAllActividades().subscribe(actividadesBd => this.actividades = actividadesBd)
+    this.http.getAllActividades().subscribe(actividadesBd => {this.actividades = actividadesBd; this.actividadesUsuario = actividadesBd})
   }
 
   abrirActividad(codigo: string){
     this.router.navigate(['/actividad', codigo])
+  }
+
+  misActividades(){
+    this.todasActividades = false
+    this.actividadesUsuario = this.actividades.filter(act => act.asistentes.includes(this.email))
+  }
+
+  todasActividadesMostrar(){
+    this.todasActividades = true
+    this.actividadesUsuario = this.actividades
   }
   
 }
